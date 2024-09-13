3f0ccb85e86032275205b4b497e0e9d040c97295
Bibhushita Baral <113716446+Mimi1101@users.noreply.github.com>
2024-09-13 11:32:35 -0500

    unique words

--- Baral_hnrs3035_cshw1.py
+++ Baral_hnrs3035_cshw1.py
@@ -8,10 +8,13 @@ class Ngram:
 
     def train(self, data):
         words = self.split_words(data)
+        lowercase = [word.lower() for word in words]
+        unique_words = set(lowercase)
 
     def split_words(self, texts):
         regex = r"\w+|[^\w\s]"
         splitted_words = re.findall(regex, texts)
+        return splitted_words
 
 
 
