2e0a96860d22d4779b286adfebc292bfee1b2374
Bibhushita Baral <113716446+Mimi1101@users.noreply.github.com>
2024-09-13 10:43:27 -0500

    splitting words done

--- Baral_hnrs3035_cshw1.py
+++ Baral_hnrs3035_cshw1.py
@@ -1,1 +1,21 @@
+import re
 
+
+class Ngram:
+
+    def __init__(self, n):
+       self.n = n  
+
+    def train(self, data):
+        words = self.split_words(data)
+
+    def split_words(self, texts):
+        regex = r"\w+|[^\w\s]"
+        splitted_words = re.findall(regex, texts)
+
+
+
+        
+
+
+  
